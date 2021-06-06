03d077b84fe8bccd973abf18c094f8a0c72a0412
fadhdwisam <fadhdwisam@gmail.com>
2021-06-06 08:59:58 +0700

    login

---
+++ src/gunawan_197064416186_uts/Gunawan_197064416186_UTS.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gunawan_197064416186_uts;
+
+/**
+ *
+ * @author BenerIt
+ */
+public class Gunawan_197064416186_UTS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
